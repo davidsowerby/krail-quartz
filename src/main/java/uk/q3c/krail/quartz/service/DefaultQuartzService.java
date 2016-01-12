bdcd218ca9f65dbdcc535689cf9ecd37309954f8
David Sowerby <david.sowerby@virgin.net>
2016-01-12 23:23:57 +0000

    using MockService based on AbstractService, Executor tests green

--- src/main/java/uk/q3c/krail/quartz/service/DefaultQuartzService.java
+++ src/main/java/uk/q3c/krail/quartz/service/DefaultQuartzService.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (C) 2013 David Sowerby
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ *
+ *  * Copyright (c) 2016. David Sowerby
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ *  * the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ *  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  * specific language governing permissions and limitations under the License.
+ *
  */
 package uk.q3c.krail.quartz.service;
 
@@ -22,6 +22,7 @@ import uk.q3c.krail.core.config.ApplicationConfigurationService;
 import uk.q3c.krail.core.eventbus.GlobalBusProvider;
 import uk.q3c.krail.core.services.AbstractService;
 import uk.q3c.krail.core.services.Dependency;
+import uk.q3c.krail.core.services.RelatedServicesExecutor;
 import uk.q3c.krail.core.services.ServicesModel;
 import uk.q3c.krail.i18n.I18NKey;
 import uk.q3c.krail.i18n.Translate;
@@ -67,8 +68,9 @@ public class DefaultQuartzService extends AbstractService implements QuartzServi
                                 Set<TriggerListenerEntry> triggerListeners, Provider<KrailSchedulerFactory>
             factoryProvider, ApplicationConfigurationService applicationConfigurationService,
                                 SchedulerProvider schedulerProvider, Injector injector, KrailJobFactory jobFactory, Set<JobEntry> jobs, Set<JobListenerEntry>
-                                            jobListeners, ServicesModel servicesModel, GlobalBusProvider globalBusProvider) {
-        super(translate, servicesModel, globalBusProvider);
+                                            jobListeners, ServicesModel servicesModel, GlobalBusProvider globalBusProvider, RelatedServicesExecutor
+                                            servicesExecutor) {
+        super(translate, servicesModel, globalBusProvider, servicesExecutor);
         this.schedulerConfigurations = schedulerConfigurations;
         this.schedulerListeners = schedulerListeners;
         this.factoryProvider = factoryProvider;
